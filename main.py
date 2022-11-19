88b2eb680c9c246d80c7f5580f8ee3c9a6c7126d
Deepali <deepalidwaghmare@yahoo.com>
2022-11-19 22:37:23 +0000

    -en implemented

---
+++ main.py
@@ -0,0 +1,78 @@
+
+p = 0
+q = 0
+cipher = "!#@ abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"
+secret_key = 0
+public_key = 0
+plain_text = ""
+encrypted_text = ""
+word = ""
+key_number = 1
+run = True
+
+while run:
+    p, q = input("Two numbers separated by colun it should be prime no:\n#>").split(" ")
+    p = int(p)
+    q = int(q)
+    n = p * q
+    z = (p-1)*(q-1)
+
+    for i in range(1, 1000):
+        if 0 != z % i:
+            public_key = round(i)
+            break
+
+    for j in range(0, 10000):
+        d = 1 + (j * z)
+        d2 = d/public_key
+        if 0 == d % public_key:
+            secret_key = round(d2)
+            break
+
+    start = True
+    while start:
+        print("p=", p)
+        print("q=", q)
+        print("modulo=", n)
+        print("publick key", public_key)
+        print("secret key", secret_key)
+        print("Commands")
+        print("-en")
+        print("-de")
+        print("exit")
+        word = input("#>")
+
+        if "-exit" in word:
+            start = False
+
+        if "-en" in word:
+            plain_text = input("ENter PT")
+            encrypted_text =""
+            for k in plain_text:
+                m = 0
+                for l in cipher:
+                    if k == l:
+                        if m < 10:
+                            m = m + 00
+                        encrypted_text = encrypted_text + (str((m ** public_key)% n))  + " "
+                        break
+                    m += 1
+            print("encrypted Text: " , encrypted_text)
+            input("press enter")
+
+        if "-de" in word:
+            encrypted_text = input("ENeter En Text: ")
+            plain_text = ""
+            for s in encrypted_text:
+                m = 0
+                for l in cipher:
+                    if s == l:
+                        if s == (str((m ** public_key) % n)):
+                            plain_text = plain_text + s
+                        break
+                    m += 1
+            print("PT: " ,plain_text)
+            input("Press Enter")
+
+        if "-exit" in word:
+            run = False
