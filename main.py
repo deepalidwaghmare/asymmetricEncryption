3174d606ba51274dfae2658cd87fc267fbf6b545
Deepali <deepalidwaghmare@yahoo.com>
2022-11-20 09:34:39 +0000

    -de implementation

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ while run:
             start = False
 
         if "-en" in word:
-            plain_text = input("ENter PT")
+            plain_text = input("Enter your plain Text: ")
             encrypted_text =""
             for k in plain_text:
                 m = 0
